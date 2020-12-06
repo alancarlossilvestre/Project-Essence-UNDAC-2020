60ee754d7853e5dfee7c836f0868d6e54e66f360
Alan Carlos <alancarlossilvestre1@gmail.com>
2020-12-06 16:41:45 -0500

    Splah Screen y login agregado, hay fallas al pulsar el boton del login exactamente al abrir la pantalla principal

--- app/src/main/java/com/example/doblenav/Login.java
+++ app/src/main/java/com/example/doblenav/Login.java
@@ -2,13 +2,23 @@ package com.example.doblenav;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+     Button ir_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ir_login = (Button) findViewById(R.id.btn_login);
+    }
+    public void ir_a_principal(View view){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
