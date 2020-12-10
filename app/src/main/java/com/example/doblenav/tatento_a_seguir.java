12df5d0cb62318c6d94391ebbf64ebce4e7a867d
ALEX HY <alessobahy9@gmail.com>
2020-12-09 19:16:14 -0500

    ADD layout talento propio y talento a seguir

---
+++ app/src/main/java/com/example/doblenav/tatento_a_seguir.java
@@ -0,0 +1,14 @@
+package com.example.doblenav;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tatento_a_seguir extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tatento_a_seguir);
+    }
+}
